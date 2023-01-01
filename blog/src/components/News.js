836e3a8b073fadf4a052c6bf216e78f0bb094b08
S.M.Samrat <freefire808709@gmail.com>
2023-01-02 00:45:38 +0600

    fetch api  and show data in fronted

--- blog/src/components/News.js
+++ blog/src/components/News.js
@@ -2,15 +2,35 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export default class News extends Component {
+    constructor(){  
+        super();
+        this.state ={
+            articles: [],
+            loading: false
+        }  
+   }  
+
+    async componentDidMount(){
+        let url ="https://newsapi.org/v2/top-headlines?country=in&apiKey=797101dc43464e9283a5d20c3c057d47";
+        let data = await fetch(url);
+        let perseData = await data.json()
+        this.setState({articles: perseData.articles})
+        console.log(perseData)
+    
+    }
   render() {
     return (
-      <div>
-        <p>this is news components</p>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-      </div>
+        <div className='container'>
+        <div className='row'>
+            {this.state.articles.map((element)=>{
+                return <div  className='col-md-4' key = {element.url}>
+                <NewsItem title={element.title} description={element.description} />
+                </div>
+            })}
+            
+        </div>
+    </div>
+
     )
   }
 }
--- blog/src/components/NewsItem.js
+++ blog/src/components/NewsItem.js
@@ -2,10 +2,16 @@ import React, { Component } from 'react'
 
 export default class NewsItem extends Component {
   render() {
+    let {title, description} = this.props
     return (
-      <div>
-        <p>This is news item component of news component 1</p>
-      </div>
+    <div className="card" style={{width: "18rem"}}>
+        <img src="..." className="card-img-top" alt="..."/>
+        <div className="card-body">
+        <h5 className="card-title">{title}</h5>
+        <p className="card-text">{description}</p>
+        <a href="/" className="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
     )
   }
 }
